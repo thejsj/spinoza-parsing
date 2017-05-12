c40e4ae92f63f03604c9485764cc7a9ebf2467c3
thejsj <jorge.silva@thejsj.com>
2017-05-11 22:02:53 -0700

    Add changes. Add result

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const fs = require('fs')
 const toArabic = require('roman-numerals').toArabic
 
 // References
-const refRegexGlobal = /([ivx]*) (Prop|Deff|Ax)\. ([ivx]*)\./ig
-const refRegex = /([ivx]*) (Prop|Deff|Ax)\. ([ivx]*)\./i
+const refRegexGlobal = /([ivx]+)\.(?:[\,\s])?(Prop|Deff|Def|Ax)?\.?(?:\s)?([ivx]+)\.( of this)?/ig
+const refRegexes = [
+  /([ivx]+)\.(?:[\,\s]*)?(Prop|Deff|Def|Ax)\.(?:\s)?([ivx]+)\./i,
+  /(Prop|Deff|Def|Ax)\.(?:[\,\s]*)?([ivx]+)\.( of this)/i,
+  /([ivx]+)\.(?:[\,\s]*)?([ivx]+)\./i,
+]
 
 const readFile = name => {
   return Promise.fromCallback(cb => {
@@ -16,15 +20,44 @@ const readFile = name => {
 }
 
 const isRomanNumber = c => c.match(/^([IVX]*)\. (.*)/) || c.match(/^PROP\. ([IVX]*)\. (.*)/)
-const isProof = c => c.match(/^ Proof\./)
+const isPartOfProp = c => c.match(/^ (Proof|Note|Corollary)\./)
 const isTitle = c => c.match(/^([A-Z]*)\./)
+
+const getRef = title => {
+  const m1 = title.match(refRegexes[0])
+  if (m1) {
+     return { partNumber: m1[1], type: m1[2], number: m1[3], regex: 1 }
+  }
+  const m2 = title.match(refRegexes[1])
+  if (m2) {
+     return { partNumber: 'current', type: m2[1], number: m2[2], regex: 2 }
+  }
+  const m3 = title.match(refRegexes[2])
+  if (m3) {
+     return { partNumber: m3[1], type: 'Prop', number: m3[2], regex: 3 }
+  }
+}
+
+const getPart = (ref, partNumber) => {
+  if (ref.partNumber === 'current') return partNumber
+  if (ref.partNumber) return toArabic(ref.partNumber)
+  return 1
+}
+
+const getType = t => {
+  if (t.match('/def/i')) return 'Def'
+  if (t.match('/ax/i')) return 'Ax'
+  return 'P'
+}
 
 const convertToId = (title, partNumber, type) => {
   if (type) {
     return 'E' + partNumber + type + toArabic(title)
   }
-  const ref = title.match(refRegex)
-  return 'E' + toArabic(ref[1] || 'I') + ref[2] + toArabic(ref[3])
+  const ref = getRef(title)
+  const part = getPart(ref, partNumber)
+  type = getType(ref.type)
+  return 'E' + part + type + toArabic(ref.number)
 }
 
 const getReferences = (text, partNumber) => {
@@ -45,12 +78,12 @@ const getNumberPoints = (text, partNumber, type) => {
         const title = match[1]
         const text = match[2]
         const id = convertToId(title, partNumber, type)
-        items.push({ /* title, text,*/ id })
+        items.push({ /* title, text,*/ id, references: [] })
         continue
       }
-      if (isProof(currentLine)) {
+      if (isPartOfProp(currentLine)) {
         // items[items.length - 1].proof = currentLine
-        items[items.length - 1].references = getReferences(currentLine, partNumber, type)
+        items[items.length - 1].references = items[items.length - 1].references.concat(getReferences(currentLine, partNumber, type))
         continue
       }
       if (currentLine.length === 0) continue
@@ -60,37 +93,37 @@ const getNumberPoints = (text, partNumber, type) => {
 }
 
 return Promise.all([
-  // readFile('./parts/1.md'),
+  readFile('./parts/1.md'),
   readFile('./parts/2.md'),
-  // readFile('./parts/3.md'),
-  // readFile('./parts/4.md'),
-  // readFile('./parts/5.md')
+  readFile('./parts/3.md'),
+  readFile('./parts/4.md'),
+  readFile('./parts/5.md')
 ])
 .map(str => {{
   const titleMatch = str.match(/PART ([IVX]*). ([A-z ]*)/i)
   const title = titleMatch[2]
   const number = toArabic(titleMatch[1])
   // Definitions
-  const hasDefinitions = str.match(/DEFINITIONS\./)
+  const hasDefinitions = str.match(/DEFINITIONS(?:\.)?\n/)
   let definitions = null
   if (hasDefinitions !== null) {
-    let restOfText = str.match(/DEFINITIONS\.([\S\s]*)/)[1]
+    let restOfText = str.match(/DEFINITIONS(?:\.)?([\S\s]*)/)[1]
     restOfText = restOfText.split('\n')
     definitions = getNumberPoints(restOfText, number, 'Def')
   }
   // Axioms
-  const hasAxioms = str.match(/AXIOMS\./)
+  const hasAxioms = str.match(/AXIOMS(?:\.)?\n/)
   let axioms = []
   if (hasAxioms!== null) {
-    let restOfText = str.match(/AXIOMS\.([\S\s]*)/)[1]
+    let restOfText = str.match(/AXIOMS(?:\.)?([\S\s]*)/)[1]
     restOfText = restOfText.split('\n')
     axioms = getNumberPoints(restOfText, number, 'Ax')
   }
   // Propositions
-  const hasPropositions = str.match(/PROPOSITIONS\./)
+  const hasPropositions = str.match(/PROPOSITIONS(?:\.)?\n/)
   let propositions = []
   if (hasAxioms!== null) {
-    let restOfText = str.match(/PROPOSITIONS\.([\S\s]*)/)[1]
+    let restOfText = str.match(/PROPOSITIONS(?:\.)?([\S\s]*)/)[1]
     restOfText = restOfText.split('\n')
     propositions = getNumberPoints(restOfText, number, 'P')
   }
